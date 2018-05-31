7ee1be482836dedb8c87573d3fe864edf751755d
Zach Owens <zowens@zowens-mn1.linkedin.biz>
2018-05-31 18:45:59 -0500

    points standings

--- data.js
+++ data.js
@@ -1,30 +1,14 @@
-{
-"golfers":{
-{
-    "name": "Justin Rose",
-    "rd1": 0,
-    "rd2": -2,
-    "rd3": "",
-    "rd4": "",
-  },
-  {
-    "name": "Jason Day",
-    "rd1": 3,
-    "rd2": -1,
+[{
+  "JR": {
+    "rd1": "0",
+    "rd2": "-2",
     "rd3": "",
     "rd4": ""
   },
-  {
-    "name": "Matt Kuchar",
-    "rd1": -4,
-    "rd2": 3,
+  "JD": {
+    "rd1": "3",
+    "rd2": "-1",
     "rd3": "",
     "rd4": ""
-  },
-  {
-    "name": "Adam Scott",
-    "rd1": 3,
-    "rd2": 1,
-    "rd3": "",
-    "rd4": ""
-  }}}
+  }
+}]
